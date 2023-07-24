7a1ab43a06d8222be6155beb3dfff990c8e5800f
Jens J <stiesel_diesel@yahoo.de>
2023-07-24 18:11:54 +0200

    user roles

--- main.py
+++ main.py
@@ -14,12 +14,16 @@ authenticator = stauth.Authenticate(
     config['cookie']['expiry_days'],
     config['preauthorized']
 )    
-name, authentication_status, username = authenticator.login('Login', 'sidebar')
 
+name, authentication_status, username = authenticator.login('Login', 'main')
 if authentication_status:
     authenticator.logout('Logout', 'main')
-    st.write(f'Welcome *{name}*')
-    st.title('Some content')
+    if username == 'jsmith':
+        st.write(f'Welcome *{name}*')
+        st.title('Application 1')
+    elif username == 'rbriggs':
+        st.write(f'Welcome *{name}*')
+        st.title('Application 2')
 elif authentication_status == False:
     st.error('Username/password is incorrect')
 elif authentication_status == None:
